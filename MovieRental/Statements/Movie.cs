9b0b15f101c334e726468379805889a0c34e2eda
John Sonneville <SonnevilleJ@gmail.com>
2014-08-06 21:17:44 -0500

    Moved movie price calculation to Movie class.

--- MovieRental/Statements/Movie.cs
+++ MovieRental/Statements/Movie.cs
@@ -15,5 +15,33 @@
         public int PriceCode { get; set; }
 
         public string Title { get; set; }
+
+        public double GetMovieAmount(double daysRented)
+        {
+            double thisAmount = 0;
+            switch (PriceCode)
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    thisAmount += getExcessMovieAmount(daysRented, 2.0);
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += daysRented * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    thisAmount += getExcessMovieAmount(daysRented, 3.0);
+                    break;
+            }
+            return thisAmount;
+        }
+
+        private static double getExcessMovieAmount(double daysRented, double dayAllotment)
+        {
+            var price = 0.0;
+            if (daysRented > dayAllotment)
+                price += (daysRented - dayAllotment) * 1.5;
+            return price;
+        }
     }
 }
