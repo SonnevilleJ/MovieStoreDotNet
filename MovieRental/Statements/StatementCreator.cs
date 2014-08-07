27b1bbea46b580b93a6d8c2c877fb083cef96928
John Sonneville <SonnevilleJ@gmail.com>
2014-08-06 21:04:52 -0500

    Moved rental amount calculation to Rental class.

--- MovieRental/Statements/StatementCreator.cs
+++ MovieRental/Statements/StatementCreator.cs
@@ -10,7 +10,7 @@
 
             foreach (var rental in customer.GetRentals())
             {
-                var thisAmount = getRentalAmount(rental);
+                var thisAmount = rental.getRentalAmount();
 
                 frequentRenterPoints++;
 
@@ -27,34 +27,5 @@
             return result;
         }
 
-        private static double getRentalAmount(Rental rental)
-        {
-            double thisAmount = 0;
-
-            var daysRented = rental.DaysRented;
-            switch (rental.Movie.PriceCode)
-            {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    thisAmount += getExcessMovieAmount(daysRented, 2.0);
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += daysRented*3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    thisAmount += getExcessMovieAmount(daysRented, 3.0);
-                    break;
-            }
-            return thisAmount;
-        }
-
-        private static double getExcessMovieAmount(double daysRented, double dayAllotment)
-        {
-            var price = 0.0;
-            if (daysRented > dayAllotment)
-                price += (daysRented - dayAllotment)*1.5;
-            return price;
-        }
     }
 }
